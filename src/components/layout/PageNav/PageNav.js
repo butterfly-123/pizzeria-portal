35232ea6caa0ea1789eff6b3f70b2b5d851e0bc9
Bombel <bombel@bombel.com>
2020-03-20 10:06:56 +0100

    Add Link to PageNav

--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
  
 const PageNav = () => 
   <nav>
     <NavLink exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>Home</NavLink>
     <NavLink to={`${process.env.PUBLIC_URL}/login`} activeClassName='active'>Login</NavLink>
+    <Link to={`${process.env.PUBLIC_URL}/ordering/new`} activeClassName='active'>Ordering</Link>
+    <Link to={`${process.env.PUBLIC_URL}/ordering/order/123abc`} activeClassName='active'>Ordering</Link>
   </nav>
  
 export default PageNav;
