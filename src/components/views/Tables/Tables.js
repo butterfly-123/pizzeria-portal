befad289864331e868a1d772c6a7f8a484b0dbb4
Bombel <bombel@bombel.com>
2020-03-20 09:06:26 +0100

    Add component properties

---
+++ src/components/views/Tables/Tables.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Tables.scss';
+
+const Tables = () => (
+  <div className={styles.component}>
+    <h2>Login view</h2>
+  </div>
+);
+
+export default Tables;
