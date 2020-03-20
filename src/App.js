343f92fec9e96e8f18efc74228b5aa709f1bd12c
Bombel <bombel@bombel.com>
2020-03-20 14:12:44 +0100

    Make button in the view component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './components/views/Login/Login';
 import Homepage from './components/views/Homepage/Homepage';
+import { Button } from '@material-ui/core';
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <MainLayout>
         <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage} />
         <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+        <Button variant='outlined' size='small' color='secondary'>Click me</Button>
       </MainLayout>
     </BrowserRouter>
   );
