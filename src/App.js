7d3629f33140c87ae637a5a5e21855f6b6ef9515
Bombel <bombel@bombel.com>
2020-03-20 12:47:24 +0100

    Install react-router-transition

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import MainLayout from './components/layout/MainLayout/MainLayout';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { Switch } from 'react-router-transition';
 import Login from './components/views/Login/Login';
 import Homepage from './components/views/Homepage/Homepage';
 
@@ -9,10 +8,8 @@ function App() {
   return (
     <BrowserRouter basename={'/panel'}>
       <MainLayout>
-        <Switch>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage} />
-          <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
-        </Switch>
+        <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage} />
+        <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
       </MainLayout>
     </BrowserRouter>
   );
