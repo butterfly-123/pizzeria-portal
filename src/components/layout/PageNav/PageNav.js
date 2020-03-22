70dfb220f76fc31304a7804e3e29395a7a72235e
Bombel <bombel@bombel.com>
2020-03-22 09:56:49 +0100

    Add new components in Tables and Ordering

--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -6,7 +6,6 @@ const PageNav = () =>
     <nav>
       <NavLink exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>Dashboard</NavLink>
       <NavLink to={`${process.env.PUBLIC_URL}/login`} activeClassName='active'>Login</NavLink>
-      <NavLink to={`${process.env.PUBLIC_URL}/homepage`} activeClassName='active'>Homepage</NavLink>
       <NavLink to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName='active'>Kitchen</NavLink>
       <NavLink to={`${process.env.PUBLIC_URL}/tables`} activeClassName='active'>Tables</NavLink>
       <NavLink to={`${process.env.PUBLIC_URL}/ordering`} activeClassName='active'>Ordering</NavLink>
