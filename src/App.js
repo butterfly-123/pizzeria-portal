86c2307499855440d72ceebfd82c8d82a698d46f
Bombel <bombel@bombel.com>
2020-03-25 20:25:34 +0100

    Make change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import MainLayout from './components/layout/MainLayout/MainLayout';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Dashboard from './components/views/Dashboard/Dashboard';
 import Kitchen from './components/views/Kitchen/Kitchen';
 import Login from './components/views/Login/Login';
@@ -17,21 +17,19 @@ import store from './redux/store';
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter basename={'/panel'}>
-        <MainLayout>
-          <Switch>
-            <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard} />
-            <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
-            <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
-            <Route exact path={process.env.PUBLIC_URL + '/ordering'} component={Ordering} />
-            <Route exact path={process.env.PUBLIC_URL + '/ordering/new'} component={OrderNew} />
-            <Route exact path={process.env.PUBLIC_URL + '/ordering/order/:id'} component={OrderingOrderID} />
-            <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
-            <Route exact path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={TablesBookingID} />
-            <Route exact path={process.env.PUBLIC_URL + '/tables/event/:id'} component={TablesEventID} />
-          </Switch>
-        </MainLayout>
-      </BrowserRouter>
+      <MainLayout>
+        <Switch>
+          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard} />
+          <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+          <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+          <Route exact path={process.env.PUBLIC_URL + '/ordering'} component={Ordering} />
+          <Route exact path={process.env.PUBLIC_URL + '/ordering/new'} component={OrderNew} />
+          <Route exact path={process.env.PUBLIC_URL + '/ordering/order/:id'} component={OrderingOrderID} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={TablesBookingID} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables/event/:id'} component={TablesEventID} />
+        </Switch>
+      </MainLayout>
     </Provider>
   );
 }
--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -4,20 +4,20 @@ import { NavLink, } from 'react-router-dom';
 const PageNav = () => 
   <div>
     <nav>
-      <button style={{ margin: 20, padding: 10, fontWeight: 'bold', textDecoration: 'none', backgroundColor: '#fff'}}>
-        <NavLink exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>DASHBOARD</NavLink> 
+      <button style={{ margin: 20, padding: 10}}>
+        <NavLink exact to={`${process.env.PUBLIC_URL}/`} style={{fontWeight: 'bold', textDecoration: 'none', backgroundColor: '#fff'}} activeClassName='active'>DASHBOARD</NavLink> 
+      </button>
+      <button style={{ margin: 20, padding: 10}}>
+        <NavLink to={`${process.env.PUBLIC_URL}/login`} style={{fontWeight: 'bold', textDecoration: 'none', backgroundColor: '#fff'}} activeClassName='active'>LOGIN</NavLink> 
       </button>
       <button style={{ margin: 20, padding: 10, fontWeight: 'bold', textDecoration: 'none'}}>
-        <NavLink to={`${process.env.PUBLIC_URL}/login`} activeClassName='active'>LOGIN</NavLink> 
+        <NavLink to={`${process.env.PUBLIC_URL}/kitchen`} style={{fontWeight: 'bold', textDecoration: 'none', backgroundColor: '#fff'}} activeClassName='active'>KITCHEN</NavLink> 
       </button>
       <button style={{ margin: 20, padding: 10, fontWeight: 'bold', textDecoration: 'none'}}>
-        <NavLink to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName='active'>KITCHEN</NavLink> 
+        <NavLink to={`${process.env.PUBLIC_URL}/tables`} style={{fontWeight: 'bold', textDecoration: 'none', backgroundColor: '#fff'}} activeClassName='active'>TABLES</NavLink> 
       </button>
       <button style={{ margin: 20, padding: 10, fontWeight: 'bold', textDecoration: 'none'}}>
-        <NavLink to={`${process.env.PUBLIC_URL}/tables`} activeClassName='active'>TABLES</NavLink> 
-      </button>
-      <button style={{ margin: 20, padding: 10, fontWeight: 'bold', textDecoration: 'none'}}>
-        <NavLink to={`${process.env.PUBLIC_URL}/ordering`} activeClassName='active'>ORDERING</NavLink> 
+        <NavLink to={`${process.env.PUBLIC_URL}/ordering`} style={{fontWeight: 'bold', textDecoration: 'none', backgroundColor: '#fff'}} activeClassName='active'>ORDERING</NavLink> 
       </button>
     </nav>
   </div>
